d9ad0b9fb2e879b5484752d34e190819be36f7b8
Shek Rasel <your-email@example.com>
2025-04-19 15:40:45 +0600

    variable name change

---
+++ projectData/projects.js
@@ -0,0 +1,68 @@
+export const projects = [
+    {id:1,
+      date: "2/4/2025",
+      logo: "/logo/project1.svg",
+      projectName: "First Project",
+      members : [
+        {
+          photo : "/images/person1.jpg",
+          name  : 'Shahed Kumar'
+        },
+        {
+          photo : "/images/person2.jpg",
+          name  : 'Mulli Proshad'
+        }
+      ],
+    },
+    {id:2,
+        date: "3/3/2025",
+        logo: "/logo/project2.svg",
+        projectName: "TaskPro Mobile App",
+        members : [
+          {
+            photo : "/images/person3.jpg",
+            name  : 'Gokkhu Master'
+          },
+          {
+            photo : "/images/person4.avif",
+            name  : 'Shakib Khan'
+          }
+        ],
+      },
+      {id:3,
+        date: "2/2/2025",
+        logo: "/logo/project3.svg",
+        projectName: "Website Development",
+        members : [
+          {
+            photo : "/images/person6.jpg",
+            name  : 'Riyadh Hasna'
+          },
+          {
+            photo : "/images/person5.jpg",
+            name  : 'Maruf VenketRau'
+          },
+          {
+            photo : "/images/person2.jpg",
+            name  : 'Rasel Pro'
+          }
+        ],
+        
+      },
+      {id:4,
+        date: "10/4/2025",
+        logo: "/logo/project4.svg",
+        projectName: "Web Design",
+        members : [
+          {
+            photo : "/images/person3.jpg",
+            name  : 'Protim Sorkar'
+          },
+          {
+            photo : "/images/person1.jpg",
+            name  : 'Soniya Mirja'
+          }
+        ],
+        
+      },
+  ];
