7d180fcb7cdc4f209e78b4b0b0e0ff20849e53b1
Shek Rasel <your-email@example.com>
2025-04-19 15:42:48 +0600

    global context api implement for adding project to the task

--- context/AddProjectContext.jsx
+++ context/AddProjectContext.jsx
@@ -1,22 +1,37 @@
-'use client'
-import React, { createContext, useContext, useState } from 'react'
+"use client";
+import React, { createContext, useContext, useState } from "react";
 
 const projectContext = createContext();
 
-
-const AddProjectContext = ({children}) => {
-const [projectName,setProjectName] = useState('');
-const [data, setDate] = useState(null);
-const[projectLogo,setProjectLogo] = useState(null);
-const[projectMember,setProjectMember] = useState('');
-const[projectMemberPhoto,setProjectMemberPhoto] = useState(null);
+const AddProjectContext = ({ children }) => {
+    const [totalProject,setTotalProject] = useState([]);
+  const [projectName, setProjectName] = useState("");
+  const [projectDate, setProjectDate] = useState(null);
+  const [projectLogo, setProjectLogo] = useState(null);
+  const [projectMember, setProjectMember] = useState("");
+  const [projectMemberPhoto, setProjectMemberPhoto] = useState(null);
   return (
-    <projectContext.Provider value={{projectName,setProjectName,data,setDate,projectLogo,setProjectLogo,projectMember,setProjectMember,projectMemberPhoto,setProjectMemberPhoto}}>
-        {children}
+    <projectContext.Provider
+      value={{
+        totalProject,
+        setTotalProject,
+        projectName,
+        setProjectName,
+        projectDate,
+        setProjectDate,
+        projectLogo,
+        setProjectLogo,
+        projectMember,
+        setProjectMember,
+        projectMemberPhoto,
+        setProjectMemberPhoto,
+      }}
+    >
+      {children}
     </projectContext.Provider>
-  )
-}
+  );
+};
 
-export default AddProjectContext
+export default AddProjectContext;
 
-export const useAddProjectContext = ()=> useContext(projectContext);
+export const useAddProjectContext = () => useContext(projectContext);
