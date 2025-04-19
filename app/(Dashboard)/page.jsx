594de9917f8a80572cefb03445e6468516934dc4
Shek Rasel <your-email@example.com>
2025-04-19 15:44:14 +0600

    dia shad cn

--- app/(Dashboard)/page.jsx
+++ app/(Dashboard)/page.jsx
@@ -1,5 +1,6 @@
 import DashboardInfo from '@/components/DashboardInfo'
 import Notes from '@/components/Notes'
+import Projects from '@/components/Projects'
 import Todo from '@/components/Todo'
 import React from 'react'
 
@@ -10,6 +11,10 @@ const page = () => {
       <div className='flex mt-8 flex-col md:flex-row gap-6 w-full'>
         <Notes/>
         <Todo/>
+      </div>
+      <div className='mt-6'>
+        <h1 className='text-2xl pb-4'>Projects</h1>
+        <Projects/>
       </div>
     </div>
   )
--- app/(Dashboard)/project/page.jsx
+++ app/(Dashboard)/project/page.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import AddProject from "@/components/AddProject";
+import Projects from "@/components/Projects";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-        project
+    <div className="">
+      <div className="mb-8 flex items-center justify-center">
+        <AddProject />
+      </div>
+      <Projects />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
