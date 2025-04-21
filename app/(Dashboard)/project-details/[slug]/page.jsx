c6f3160358b9a697294d9128e0e59f11d9d711c8
Shek Rasel <your-email@example.com>
2025-04-21 23:44:48 +0600

    project details page initialize

--- app/(Dashboard)/project-details/[slug]/page.jsx
+++ app/(Dashboard)/project-details/[slug]/page.jsx
@@ -1,9 +1,15 @@
+import TaskProgress from '@/components/TaskProgress';
 import React from 'react'
 
-const page = () => {
+const page = async ({params}) => {
+const {slug} = await params;
+ const page = decodeURIComponent(slug);
   return (
-    <div>
-        details page
+    <div className='w-full'>
+        <h1 className='text-lg font-semibold'>{page}</h1>
+        <div className='mt-4'>
+        <TaskProgress/>
+        </div>
     </div>
   )
 }
