bbc6c48815cdac2be66098a835dbb7272154d4c1
Shek Rasel <your-email@example.com>
2025-04-21 14:43:26 +0600

    project delete functionality done

--- components/DeleteProject.jsx
+++ components/DeleteProject.jsx
@@ -1,6 +1,18 @@
+"use client";
 import React from "react";
-import { DropdownMenu,DropdownMenuContent, DropdownMenuItem,DropdownMenuTrigger } from "./ui/dropdown-menu";
-const DeleteProject = () => {
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+const DeleteProject = ({ projectId, updatedProjects, setUpdatedProjects }) => {
+  const deleteSingleProject = () => {
+    const afterDeleteProjects = updatedProjects.filter((deleteProject) => {
+      return deleteProject.id !== projectId;
+    });
+    setUpdatedProjects(afterDeleteProjects);
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className=" border-none outline-none focus:outline-none focus:ring-0 cursor-pointer">
@@ -9,7 +21,16 @@ const DeleteProject = () => {
       <DropdownMenuContent className="bg-transparent border-none shadow-none">
         <div className=" bg-white border text-black rounded-md mt-2 py-2 mr-7 md:mr-22">
           <DropdownMenuItem className="px-10">Archieve</DropdownMenuItem>
-          <DropdownMenuItem className="px-10">Delete</DropdownMenuItem>
+          <DropdownMenuItem
+            className="px-10"
+            onClick={(e) => {
+              e.stopPropagation(); 
+              e.preventDefault(); 
+              deleteSingleProject();
+            }}
+          >
+            Delete
+          </DropdownMenuItem>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,25 +1,31 @@
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { projects } from "@/projectData/projects";
 import Image from "next/image";
 import Link from "next/link";
 import DeleteProject from "./DeleteProject";
 import { useAddProjectContext } from "@/context/AddProjectContext";
 const Projects = () => {
+  const [updatedProjects,setUpdatedProjects] = useState([]); 
 const {totalProject} = useAddProjectContext();
 
-const updateTotalProject = [...projects,...totalProject];
+
+
+useEffect(()=>{
+  const updateTotalProject = [...projects,...totalProject];
+  setUpdatedProjects(updateTotalProject);
+},[totalProject])
 
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {updateTotalProject.map((project) => (
+      {updatedProjects.map((project) => (
         <Link key={project.id} href={`/project-details/${project.projectName}`}>
           <div className="w-full border p-4 rounded-md">
             <div className="flex justify-between items-center">
               <span className="font-semibold">{project.date}</span>
               <span className="font-bold">
-                <DeleteProject />
+                <DeleteProject projectId={project.id} updatedProjects = {updatedProjects} setUpdatedProjects={setUpdatedProjects}/>
               </span>
             </div>
             <div className="flex flex-col items-center justify-center w-full gap-4 mt-2">
