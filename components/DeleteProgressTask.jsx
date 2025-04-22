07f1afdabf5b98aba366de7de23ae161d913b30c
Shek Rasel <your-email@example.com>
2025-04-22 10:00:01 +0600

    progress task delete functionality implement

---
+++ components/DeleteProgressTask.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { toast } from "sonner"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+const DeleteProgressTask = ({
+  setTaskTimeLine,
+  taskTimeLine,
+  singleTask,
+  index,
+}) => {
+  const deleteProgressTask = () => {
+    const newProgressTask = taskTimeLine.filter((filterTask) => {
+      return filterTask !== singleTask;
+    });
+    setTaskTimeLine(newProgressTask);
+    toast.success('Deleted successfully!', {
+        className: 'text-green-600 font-semibold',
+      });
+  };
+  //
+  return (
+    <DropdownMenu >
+      <DropdownMenuTrigger className="border-none outline-none focus:outline-none focus:ring-0 font-bold cursor-pointer">...</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={deleteProgressTask}>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default DeleteProgressTask;
