fb8b91856f7ac6f90a02c95fb2e87ba1ea49cbe2
Shek Rasel <your-email@example.com>
2025-04-22 23:44:28 +0600

    progress task context api added

--- app/(Dashboard)/project-details/[slug]/page.jsx
+++ app/(Dashboard)/project-details/[slug]/page.jsx
@@ -1,17 +1,23 @@
-import TaskProgress from '@/components/TaskProgress';
-import React from 'react'
+import TaskProgress from "@/components/TaskProgress";
+import AddCustomizeProjectContext from "@/context/AddCustomizeProjectContext";
 
-const page = async ({params}) => {
-const {slug} = await params;
- const page = decodeURIComponent(slug);
+import React from "react";
+
+const page = async ({ params }) => {
+  const { slug } = await params;
+  const page = decodeURIComponent(slug);
   return (
-    <div className='w-full'>
-        <h1 className='text-lg font-semibold'>{page}</h1>
-        <div className='mt-4'>
-        <TaskProgress/>
-        </div>
+        
+    <div className="w-full">
+      <h1 className="text-lg font-semibold">{page}</h1>
+      <div className="mt-4">
+      <AddCustomizeProjectContext>
+          <TaskProgress />
+      </AddCustomizeProjectContext>
+      </div>
     </div>
-  )
-}
+        
+  );
+};
 
-export default page
+export default page;
