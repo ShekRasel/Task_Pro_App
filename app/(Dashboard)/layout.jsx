a8f8a97f64956597c0ca5f4587a5234b06a86095
Shek Rasel <swe.rasel@gmail.com>
2025-08-28 11:44:54 +0600

    system theme add

--- app/(Dashboard)/layout.jsx
+++ app/(Dashboard)/layout.jsx
@@ -4,12 +4,10 @@ import React, { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import AddProjectContext from "@/context/AddProjectContext";
 import { useTheme } from "next-themes";
-import { ThemeProvider } from "@/context/ThemeContext";
 
 const layout = ({ children }) => {
-  const { theme } = useTheme();
- console.log(theme,'theme');
- const [mounted, setMounted] = useState(false);
+  const { theme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
@@ -21,21 +19,21 @@ const layout = ({ children }) => {
 
   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
-      <AddProjectContext>
-        <div
-          className={`flex h-screen ${
-            currentTheme === "dark" ? "bg-[#3D434C]" : "bg-[#ECF2F4]"
-          }`}
-        >
-          <Sidebar />
-          <div className="flex flex-col flex-1 md:mt-4 overflow-hidden">
-            <Header />
-            <main className="flex-1 py-4 overflow-y-auto px-3 md:px-10 xl:px-16 2xl:px-20">
-              {children}
-            </main>
-          </div>
+    <AddProjectContext>
+      <div
+        className={`flex h-screen ${
+          currentTheme === "dark" ? "bg-[#3D434C]" : "bg-[#ECF2F4]"
+        }`}
+      >
+        <Sidebar />
+        <div className="flex flex-col flex-1 md:mt-4 overflow-hidden">
+          <Header />
+          <main className="flex-1 py-4 overflow-y-auto px-3 md:px-10 xl:px-16 2xl:px-20">
+            {children}
+          </main>
         </div>
-      </AddProjectContext>
+      </div>
+    </AddProjectContext>
   );
 };
 
